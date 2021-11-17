740fa7b87af4bbf51ad86577f9e6923f1d55b218
codeuiandy <frostandy41@gmail.com>
2021-11-17 19:20:19 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import React, { useState,useEffect } from "react";
-import './App.css';
-import Home from './home/index'
+import React from "react";
+import "./App.css";
+import Home from "./home/index";
 function App() {
-
-  const [loading,setLoading]=useState(true)
-  // if (document.readyState !== "complete") {
-  //   setLoading(true)
-  // }
-
-  useEffect(() => {
-    window.addEventListener('load', function() {
-      setLoading(false)
-  })
-  }, [])
- 
-
-
-  return (
-  
-      <Home/>
-    
-
-  );
+  return <Home />;
 }
 
 export default App;
